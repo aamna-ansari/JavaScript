496033f317c1d2c534abb9d85ac6450fed31b943
aamna-ansari <aamnansari29@gmail.com>
2024-09-24 15:12:34 +0500

    01_arrays.js

--- 02_Basic/01_arrays.js
+++ 02_Basic/01_arrays.js
@@ -1,4 +1,4 @@
-//array
+//Array
 
 // let myarr = new Array(1,2,3,4,5) //declaring arrays with Array constructor
 
@@ -7,28 +7,28 @@ const myarr = [1, 2, 3, 4, 5]
 // console.log(myarr[0]); // arrays' value can be accessed using index inside square brackets "[]"
 
 
-// myarr.push(`Payan Saleem`);
-// console.log(myarr);
+myarr.push(`AaMna`); 
+console.log(myarr);
 
 
-// myarr.pop();
-// console.log(myarr);
+myarr.pop();  // Remove last one item from array 
+console.log(myarr);
 
 
-// console.log(myarr.includes(5)); Determines whether an array includes a certain element, returning true or false as appropriate.
+console.log(myarr.includes('AaMna')); // Determines whether an array includes a certain element, returning true or false as appropriate.
 
 
-// console.log(myarr.toLocaleString()); Returns a string representation of an array. The elements are converted to string using their toLocaleString methods.
-
-
-
-// (myarr.unshift(0));  Inserts new elements at the start of an array, and returns the new length of the array.
-// console.log(myarr); 
+console.log(myarr.toLocaleString()); // eturns a string representation of an array. The elements are converted to string using their toLocaleString methods.
 
 
 
-// myarr.shift(); Removes the first element from an array and returns it. If the array is empty, undefined is returned and the array is not modified.
-// console.log(myarr); 
+(myarr.unshift(0));  //Inserts new elements at the start of an array, and returns the new length of the array.
+console.log(myarr); 
+
+
+
+myarr.shift(); // Removes the first element from an array and returns it. If the array is empty, undefined is returned and the array is not modified.
+console.log(myarr); 
 
 // let myarr2 = myarr.join() //Adds all the elements of an array into a string, separated by the specified separator string. //type: string
 // console.log(myarr); 
@@ -37,9 +37,9 @@ const myarr = [1, 2, 3, 4, 5]
 
 
 
-// console.log(myarr.slice(1,3)); //Returns a copy of a section of an array. 
-// console.log(myarr);
+console.log(myarr.slice(1,3)); //Returns a copy of a section of an array. 
+console.log(myarr);
 
 
-// console.log(myarr.splice(1,3)); //Removes elements from an array
-// console.log(myarr);
+console.log(myarr.splice(1,3)); //Removes elements from an array
+console.log(myarr);
