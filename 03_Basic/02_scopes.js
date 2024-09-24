92125ed17032829576361b8e1a4852d53a3279a5
aamna-ansari <aamnansari29@gmail.com>
2024-09-24 15:19:47 +0500

    02_scopes.js

---
+++ 03_Basic/02_scopes.js
@@ -0,0 +1,34 @@
+// let myName = "AaMna"
+// const id = 785603
+// var age = 24
+
+// {}    scope
+
+// console.log(myName);
+// console.log(id);
+// console.log(age);
+
+//////////////////////////////////
+
+// if (true) {
+//     let myName = "AaMna"
+//     const id = 785603
+//     var age = 24
+// }
+
+
+// console.log(myName);
+// console.log(id);
+// console.log(age);
+
+
+
+    // let myCity = "Faisalabad"
+
+
+    // if (myCity === "Faisalabad") {
+    //     console.log("City Is Faisalabad");
+    // } else {
+    //     console.log("City Is not Faisalabad");
+
+    // }
