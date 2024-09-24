add4781b0633be9cfed25d5c01f6f6eca1a142ef
aamna-ansari <aamnansari29@gmail.com>
2024-09-24 14:34:05 +0500

    02_data-types

---
+++ 01_Basic/02_data-types.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all Javscript code as newer version
+
+
+// alert("hello"); //we are using nodeJS not browser
+
+//data types
+
+let name = "AaMna";
+let age = 24;
+let netWorth = 119^73;
+let isLoggedIn = true;
+let wallet = null; /*Haha anyways, null value has an object dataType by Default*/
+let state;
+
+
+//string - commonly used
+// number - commonly used
+//bigint - less used
+// boolean - true or false
+// null - it's a dataType and also a standalone value , Also null is object , its big error
+// undefined - when value is not defined
+//Symbol
+//objects
+//arrays
