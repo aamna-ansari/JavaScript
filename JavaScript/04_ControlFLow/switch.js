2724600adbee292ea12efda7c451abfe71dfc8c7
aamna-ansari <aamnansari29@gmail.com>
2024-09-24 21:35:15 +0500

    switch

---
+++ JavaScript/04_ControlFLow/switch.js
@@ -0,0 +1,54 @@
+// let month = 6
+
+// switch (month) {
+//     case 1:
+//         console.log("Month is January");
+//         break;
+//     case 2:
+//         console.log("Month is February");
+//         break;
+//     case 3:
+//         console.log("Month is March");
+//         break;
+//     case 4:
+//         console.log("Month is April");
+//         break;
+//     case 5:
+//         console.log("Month is May");
+//         break;
+//     case 6:
+//         console.log("Month is June");
+//         break;
+
+//     default:
+//         console.log("Month is");
+//         break;
+// }
+
+
+
+
+
+let Name = "AaMna"
+
+switch (Name) {
+    case "ABC":
+        console.log("Your Name is ABC");
+        break;
+    case "DEF":
+        console.log("MYour Name is DEF");
+        break;
+    case "GHI":
+        console.log("Your Name is GHI");
+        break;
+    case "JKL":
+        console.log("Your Name is JKL");
+        break;
+    case "MNO":
+        console.log("Your Name is MNO");
+        break;
+
+    default:
+        console.log("You seem new");
+        break;
+}
