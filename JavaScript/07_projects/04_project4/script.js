0b8042c39dc5ff70016901e867d916dacdc89822
aamna-ansari <aamnansari29@gmail.com>
2024-09-27 14:33:49 +0500

    Project4

---
+++ JavaScript/07_projects/04_project4/script.js
@@ -0,0 +1,28 @@
+let randomNumber = parseInt(Math.random() * 100 + 1);
+let userInput = document.querySelector('#guessField');
+let submit = document.querySelector('#submit');
+let guessSlot = document.querySelector('.previousGuesses');
+let remaining = document.querySelector('.remainGuesses');
+let lowOrHi = document.querySelector('.lowOrHi');
+let startOver = document.querySelector('.resultParas');
+let p = document.createElement('p');
+
+
+let prevGuessArr = [];
+let numGuess = 1;
+let playGame = true;
+
+function validateGuess(guess){
+// check  1 to 100 valid or not 
+}
+
+function checkGuess(guess){
+    
+}
+function displayGuess(guess){
+
+}
+
+function displayMessage(message){
+
+}
