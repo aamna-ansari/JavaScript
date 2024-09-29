61ee0462b3f59e8117ce962a969fbb19058edd15
aamna-ansari <aamnansari29@gmail.com>
2024-09-29 18:09:14 +0500

    prommise Three

--- JavaScript/10_promises/01_promise.js
+++ JavaScript/10_promises/01_promise.js
@@ -1,24 +1,36 @@
 //  Create Promise 
 
 
-let promiseOne = new Promise(function(resolve, reject){
-    setTimeout(function(){
-        console.log('Async task is Complete');
-        resolve()
-    },1000)
-})
+// let promiseOne = new Promise(function(resolve, reject){
+//     setTimeout(function(){
+//         console.log('Async task is Complete');
+//         resolve()
+//     },1000)
+// })
 
-promiseOne.then(function(){
-    console.log("Promise Created");
+// promiseOne.then(function(){
+//     console.log("Promise Created");
     
+// })
+
+// ----------------  WITHOUT VARIABLE -------------
+
+// new Promise(function(resolve, reject){
+//     console.log("Its Second method of promise");
+//     resolve()
+// }).then(function(){
+//     console.log("Created")
+// })
+
+//  -------------- PROMISE THREE ---------------
+let threePromise = new Promise(function(resolve,reject){
+setTimeout(function(){
+    resolve({username: "AaMna" , email : "practicejs@gmail.com"})
+},1000)
+
 })
 
-
-// WITHOUT VARIABLE
-
-new Promise(function(resolve, reject){
-    console.log("Its Second method of promise");
-    resolve()
-}).then(function(){
-    console.log("Created")
+threePromise.then(function(user){
+    console.log(user);
+    
 })
