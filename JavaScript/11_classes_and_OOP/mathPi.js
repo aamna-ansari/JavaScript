ba75f35bbb433c4d7b7a90faef679c7f5d7e2a46
aamna-ansari <aamnansari29@gmail.com>
2024-10-01 21:31:16 +0500

    PropertyDescriptors

--- JavaScript/11_classes_and_OOP/mathPi.js
+++ JavaScript/11_classes_and_OOP/mathPi.js
@@ -3,15 +3,36 @@
 // console.log(Math.PI); //Value is not changed coz it is constant but we can change its value hehe. Let's see.
 
 
-let Descripter = Object.getOwnPropertyDescriptor(Math, 'PI')
-console.log(Descripter);
+// let Descripter = Object.getOwnPropertyDescriptor(Math, 'PI')
+// console.log(Descripter);
 
 let user = {
     firstName : 'AaMna',
     lastName: 'AnSari',
     email : 'aamna@gmail.com',
-    password : '1234'
+    password : '1234',
+    getVerification : function(){
+        console.log("Please Verify yourself📍")
+    }
 
 }
 
-console.log(user);
+// console.log(Object.getOwnPropertyDescriptor(user, 'firstName'));
+// Object.defineProperty(user , 'firstName',{
+//     writable: false,
+//     enumerable: false
+// } )
+
+// console.log(Object.getOwnPropertyDescriptor(user, 'firstName'));
+
+
+for (let [key, value] of Object.entries(user)){
+
+    // let's add a check that if value is not a type of function then iterate, otherwise do not.
+    if (typeof value !== 'function')
+        console.log(`${key}: ${value}`);
+    
+}
+
+
+
