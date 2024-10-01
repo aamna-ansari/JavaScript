6499cdd56040070bff5c88ac72b226339967e02e
aamna-ansari <aamnansari29@gmail.com>
2024-10-01 19:49:17 +0500

    ClassDone

--- JavaScript/11_classes_and_OOP/myClass.js
+++ JavaScript/11_classes_and_OOP/myClass.js
@@ -1,23 +1,51 @@
 // ES6 is current version of Javascript
 
 
-class User {
-    constructor(username, email, password) {
-        this.username = username
-        this.email = email
-        this.password = password
+// class User {
+//     constructor(username, email, password) {
+//         this.username = username
+//         this.email = email
+//         this.password = password
+//     }
+
+//     encryptPassword() {
+//         return `${this.password}abc`
+//     }
+
+//     capitalizeUser() {
+//         return `${this.username.toUpperCase()}`
+//     }
+// }
+
+// let AaMna = new User("AaMna AnSari", "aamna@h.com", "abc123?",)
+// console.log(AaMna);
+// console.log(AaMna.encryptPassword());
+// console.log(AaMna.capitalizeUser());
+
+
+
+// ----PRACTICE ----------
+
+class StaffInfo{
+    constructor(name,id, qulification, yearsOfEx, salary){
+        this.name = name;
+        this.id = id;
+         this.qulification = qulification;
+         this.yearsOfEx = yearsOfEx;
+         this.salary = salary
     }
 
-    encryptPassword() {
-        return `${this.password}abc`
+    nameFormat(){
+        return `${this.name.toUpperCase()}`
     }
 
-    capitalizeUser() {
-        return `${this.username.toUpperCase()}`
+    bouns(){
+        return `You get bonuns on your ${this.salary * 4}`
     }
 }
 
-let AaMna = new User("AaMna AnSari", "aamna@h.com", "abc123?",)
-console.log(AaMna);
-console.log(AaMna.encryptPassword());
-console.log(AaMna.capitalizeUser());
+let user = new  StaffInfo('AaMna','2024', 'BSCS', '8', '20000')
+console.log(user);
+console.log(user.bouns());
+
+
