14869f6503ee545b60f902d8ebb62c0d9fe7b91a
aamna-ansari <aamnansari29@gmail.com>
2024-10-01 19:36:11 +0500

    class

--- JavaScript/11_classes_and_OOP/myClass.js
+++ JavaScript/11_classes_and_OOP/myClass.js
@@ -1,22 +1,3 @@
-// ES6 is current version of Javascript
+// 
 
-class User{
-    constructor(username, email, password){
-        this.username = username;
-        this.email = email;
-        this.password = password
-    }
-
-    encryptPassword(){
-        return`${this.password}abc`
-    }
-
-    changeUser(){
-        return `${this.username.toUpperCase()}`
-    }
-
-}
-
-const chai = new User ('Chai', 'milk@gmail.com', '123');
-console.log(chai.encryptPassword());
-console.log(chai.changeUser());
+// Behind the scene
