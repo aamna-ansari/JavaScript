e72817dbff90bdab81317e660d9f798cb19adfdf
aamna-ansari <aamnansari29@gmail.com>
2024-10-01 19:38:29 +0500

    classess

--- JavaScript/11_classes_and_OOP/myClass.js
+++ JavaScript/11_classes_and_OOP/myClass.js
@@ -1,3 +1,23 @@
-// 
+// ES6 is current version of Javascript
 
-// Behind the scene
+
+class User {
+    constructor(username, email, password) {
+        this.username = username
+        this.email = email
+        this.password = password
+    }
+
+    encryptPassword() {
+        return `${this.password}abc`
+    }
+
+    capitalizeUser() {
+        return `${this.username.toUpperCase()}`
+    }
+}
+
+let AaMna = new User("AaMna AnSari", "aamna@h.com", "abc123?",)
+console.log(AaMna);
+console.log(AaMna.encryptPassword());
+console.log(AaMna.capitalizeUser());
