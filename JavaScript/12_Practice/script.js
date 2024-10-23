2a93f844826ce2c574e3118bbce144098c6716f9
aamna-ansari <aamnansari29@gmail.com>
2024-10-23 14:55:08 +0500

    Prototype

--- JavaScript/12_Practice/script.js
+++ JavaScript/12_Practice/script.js
@@ -3,3 +3,21 @@ y = 10;
 
 // expression 
  5 + 1;
+
+
+//  prototype 
+
+// create a function
+function Person(name, age){
+    this.name = name;
+    this.age = age;
+}
+
+Person.prototype.greet = function(){
+    return('Welocome to the prototype'); 
+}
+
+let person1 = new Person('AaMna', '24')
+
+console.log(person1.greet());
+ person1.greet();
