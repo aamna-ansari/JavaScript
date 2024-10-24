1ae9d9bf81efd562e388ebcf9863f30de106f3b9
aamna-ansari <aamnansari29@gmail.com>
2024-10-24 20:23:12 +0500

    reduce()

--- JavaScript/12_Practice/index.js
+++ JavaScript/12_Practice/index.js
@@ -11,3 +11,9 @@ console.log(numbers2)
 let nums = [1,2,3,4,5,6,7,8];
 let num2 = nums.filter(num => num % 2 !== 0);
 console.log(num2);
+
+// reduce()
+let number = [1,2,3,4,5]
+let number3 = number.reduce((accumulator, current) => accumulator + current, 0);
+console.log(number3);
+
