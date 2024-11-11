7a502b1b3f8d2a1fcca71d5f8b58176421363507
aamna-ansari <aamnansari29@gmail.com>
2024-11-11 14:56:58 +0500

    CreateUser

--- JavaScript/12_LocalStorage/script.js
+++ JavaScript/12_LocalStorage/script.js
@@ -1,1 +1,25 @@
-let inputField = document.getElementById('inputField');
+
+
+let hanldleCreate = ()=> {
+    let user = document.getElementById('inputField').value;
+    const users = JSON.parse(localStorage.getItem('users')) || [];
+    users.push('user',user)
+
+    // localStorage.setItem("user", user); //return string
+    
+    
+    localStorage.setItem("users", JSON.stringify(users)); //return Array 
+
+}
+let handleRead = ()=>{
+
+    console.log(users);
+    
+
+}
+
+
+let handleDelete = ()=>{
+    
+
+}
