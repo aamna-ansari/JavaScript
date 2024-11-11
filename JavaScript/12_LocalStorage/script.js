c8bb5ffb12b5b4561a97f1987eb709d918efa5b7
aamna-ansari <aamnansari29@gmail.com>
2024-11-11 16:01:34 +0500

    remove and read done

--- JavaScript/12_LocalStorage/script.js
+++ JavaScript/12_LocalStorage/script.js
@@ -2,7 +2,8 @@
 
 let hanldleCreate = ()=> {
     let user = document.getElementById('inputField').value;
-    const users = JSON.parse(localStorage.getItem('users')) || [];
+    const users = JSON.parse(localStorage.getItem('users')) || [];  // JSON.parse mean change into oginal form like array to string => now String to array
+
     users.push(user)    //[].push(inputfield data)
 
     // localStorage.setItem("user", user); //return Array 
@@ -12,14 +13,19 @@ let hanldleCreate = ()=> {
 
 }
 let handleRead = ()=>{
+    let dataRead = document.getElementById('dataRead')
+    let readData = JSON.parse(localStorage.getItem('users')) || [];
+    dataRead.innerHTML = readData.join(', '); 
 
-    console.log(users);
-    
+    let itemCount = document.getElementById('itemCount');
+    let countItem = localStorage.length
+    itemCount.innerHTML = countItem
 
 }
 
 
 let handleDelete = ()=>{
+    localStorage.removeItem('users')
     
 
 }
